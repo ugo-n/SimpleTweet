7a6a9da6a2a511c27c5d83b7d9736718a04fc2ff
ugo-n <eagle.nnakwe@gmail.com>
2022-03-19 01:24:24 -0400

    post tweets to timeline

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.kt
@@ -2,14 +2,19 @@ package com.codepath.apps.restclienttemplate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class ComposeActivity : AppCompatActivity() {
 
     lateinit var etCompose: EditText
     lateinit var btnTweet: Button
+
+    lateinit var client: TwitterClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +22,8 @@ class ComposeActivity : AppCompatActivity() {
 
         etCompose = findViewById(R.id.etTweetCompose)
         btnTweet = findViewById(R.id.btnTweet)
+
+        client = TwitterApplication.getRestClient(this)
 
         //Handling the user's click on the tweet button
         btnTweet.setOnClickListener{
@@ -33,11 +40,28 @@ class ComposeActivity : AppCompatActivity() {
                 Toast.makeText(this, "Tweet is too long! Limit is 140 characters", Toast.LENGTH_SHORT)
                     .show()
             } else {
-                // Make an api call to Twitter to publish tweet
-                Toast.makeText(this, tweetContent, Toast.LENGTH_SHORT)
-                    .show()
+                client.publishTweet(tweetContent, object : JsonHttpResponseHandler(){
+                    override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON?) {
+                    Log.i(TAG, "Successfully published tweet")
+                    // TODO Send the tweet back to TimelineActivity
+                    }
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Headers?,
+                        response: String?,
+                        throwable: Throwable?
+                    ) {
+                        Log.e(TAG, "Failed to publish tweet", throwable)
+                    }
+
+
+
+                })
             }
 
         }
+    }
+    companion object{
+        val TAG = "ComposeActivity"
     }
 }
